a53215ad75a9809e8462e59553e47be087d8c532
Jose Diaz <jose.diaz@joedayz.pe>
2016-10-15 22:38:54 -0500

    adaptador

---
+++ app/src/main/java/pe/joedayz/micalculadora/adapter/TipAdapter.java
@@ -0,0 +1,61 @@
+package pe.joedayz.micalculadora.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import pe.joedayz.micalculadora.R;
+import pe.joedayz.micalculadora.model.TipRecord;
+
+/**
+ * Created by josediaz on 10/15/16.
+ */
+
+public class TipAdapter extends RecyclerView.Adapter<TipAdapter.ViewHolder> {
+    Context context;
+    List<TipRecord> dataset;
+
+
+    public TipAdapter(Context contex, List<TipRecord> dataset){
+        this.context = contex;
+        this.dataset = dataset;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        TipRecord record = dataset.get(position);
+        String strTip = String.format(context.getString(R.string.global_message_tip), record.getTip());
+        holder.txtContent.setText(strTip);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.txtContent)
+        TextView txtContent;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
